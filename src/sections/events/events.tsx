4634a75686be067980ca2ad3eaa3d717395a4204
mr-vishnuv <vishnuvenugopal844@gmail.com>
2023-08-03 10:32:50 +0530

    Evet updation

--- src/sections/events/events.tsx
+++ src/sections/events/events.tsx
@@ -6,7 +6,7 @@ export default function Events(){
             <div id="Events" className="pt-32">
             <Headings textFirst="UPCOMING" textsecond="EVENTS" xFirst={163.34} xThird={14.59} contWidth="w-[288px]"/>
             <p className="w-[min(80vw,647px)] mt-6 text-center text-ms font-extralight text-[#666] mx-auto">Come join us in these exciting events!</p>
-            <Eventcard id="CIR-Q" heading="online circuit designing competition" time="Submit before 14-05-2023" text="IEEE SB RAS  GECI is here with ' CIR-Q ',an online circuit designing competition.Bring out the inner circuit designer in you and munch on this opportunity to bag prizes worth ₹1000.Online registration will start today( 12-05-2023)" month="MAY" day="14" poster="Cia.jpeg" url="https://bit.ly/3LWsnwb"/>
+            <Eventcard id="python" heading="Introduction to Python" time="07:00 pm" text="Hey folks✨, IEEE CS SBC GECI as a part of Hour Of Code 2.0 is organizing a talk session on Introduction To Python 🐍 🌟 Ignite Your Coding Passion! 🐍 Join Our Python Class Session. Unleash Python's Magic. Classes, Objects, Expert Instructors. Register Now❗" month="MAY" day="23" poster="python.jpg" url="https://bit.ly/3LWsnwb"/>
             </div>
         </>
     )
